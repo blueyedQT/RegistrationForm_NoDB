8c003a8a518fb1b9f7cb951e00a84e480d5cd391
Katrina Sanford <blueyed_qt@hotmail.com>
2014-11-14 17:32:40 -0800

    nearly done!

--- process.php
+++ process.php
@@ -1,55 +1,70 @@
 <?php
 session_start();
-var_dump($_POST);
-var_dump($_SESSION);
-var_dump($_FILES);
 
-$_SESSION['errors'] = array();
+$okay = array();
 
 if(!empty($_POST)) {
 	if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) == false) {
 		$error = "You did not enter in a valid email address!";
-		$_SESSION['errors'][] = $error;
+		$_SESSION['e'] = 'class="red"> <small>' . $error . '</small><';
+	} else {
+		$_SESSION['e'] = 'value="' . $_POST['email'] . '"><';
+		$okay['e'] = true;
 	}
+
 	if(strlen($_POST['first_name']) < 2 || is_numeric($_POST['first_name'])) {
 		$error = "You did not enter in a valid first name!";
-		$_SESSION['errors'][] = $error;
+		$_SESSION['fn'] = 'class="red"> <small>' . $error . '</small><';
+	} else {
+		$_SESSION['fn'] = 'value="' . $_POST['first_name'] . '"><';
+		$okay['fn'] = true;
 	}
+
 	if(strlen($_POST['last_name']) < 2 || is_numeric($_POST['last_name'])) {
 		$error = "You did not enter in a valid last name!";
-		$_SESSION['errors'][] = $error;
+		$_SESSION['ln'] = 'class="red"> <small>' . $error . '</small><';
+	} else {
+		$_SESSION['ln'] = 'value="' . $_POST['last_name'] . '"><';
+		$okay['ln'] = true;
 	}
-	if (strlen($_POST['password']) < 6) {
-		$error = "You did not enter in a valid password!";
-		$_SESSION['errors'][] = $error;
-	}
-	if ($_POST['password'] !== $_POST['password2']) {
-		$error = "Your password entries do not match";
-		$_SESSION['errors'][] = $error;
-	}
-	// if (!empty($_POST['birthdate'])) {
-	// 	$date = explode('-', $_POST['birthdate'], 2);
-	// 	var_dump($date);
-	// 	$dates = implode(',', $date);
-	// 	var_dump($dates);
 
-	// 	// var_dump(checkdate(foreach($date as $dates) {
-	// 	// 	$date;
-	// 	// });
-	// 	// if(checkdate($date2) != true) {
-	// 	// $error = "Your birthday was not a vaild entry";
-	// 	// $_SESSION['errors'][] = $error;
-	// 	// }
-	// }
+	if (strlen($_POST['password']) < 6 || ($_POST['password'] !== $_POST['password2'])) {
+		$error = "You did not enter in a valid password!";
+		$_SESSION['p'] = 'class="red"> <small>' . $error . '</small><';
+	} else {
+		$okay['p'] = true;
+	}
+
+	if (!empty($_POST['birthdate'])) {
+		$date = explode('-', $_POST['birthdate'], 3);
+		if(!empty($date[2])) {
+			if(checkdate($date[0], $date[1], $date[2])) {
+				$_SESSION['b'] = 'value="'. $_POST['birthdate'] . '"><';
+				$okay['b'] = true;
+			} 
+		} else {
+			$error = "Your birthday was not a vaild entry";
+			$_SESSION['b'] = 'class="red">' . $error . '<';
+			$okay['b'] = false;
+		}
+	} else {
+		$okay['b'] = true;
+	}
+
 	if(!empty($_FILES['picture'])) {
 		$target_dir = "uploads/";
 		$target_file = $target_dir . basename($_FILES['picture']['name']);
 		if(move_uploaded_file($_FILES['picture']['tmp_name'], $target_file)) {
-			$error = 'The file has been uploaded!';
+			$okay['pic'] = true;
 		} else {
-			$error = 'Error!';
+			$okay['pic'] = false;
 		}
-		$_SESSION['errors'][] = $error;
+	} else {
+		$okay['pic'] = true;
+	}
+
+	if(($okay['e'] && $okay['fn'] && $okay['ln'] && $okay['p']) && ($okay['d'] !== false || $okay['i'] !== false)) {
+		$_SESSION['message'] = 'You have registered!';
 	}
  	header('location: registration.php');
 } 
--- registration.php
+++ registration.php
@@ -1,6 +1,21 @@
 <?php 
 session_start();
-var_dump($_SESSION);
+
+if(empty($_SESSION['e'])) {
+	$_SESSION['e'] = '><';
+}
+if(empty($_SESSION['fn'])) {
+	$_SESSION['fn'] = '><';
+}
+if(empty($_SESSION['ln'])) {
+	$_SESSION['ln'] = '><';
+}
+if(empty($_SESSION['p'])) {
+	$_SESSION['p'] = '><';
+}
+if(empty($_SESSION['b'])) {
+	$_SESSION['b'] = '><';
+}
 ?>
 <!doctype html>
 <html>
@@ -11,24 +26,30 @@ var_dump($_SESSION);
 </head>
 <body>
 	<?php 
-	if(isset($_SESSION['errors'])) {
-		foreach ($_SESSION['errors'] as $error) {
-			echo "<p class='error'>" . $error . '</p>';
-		}
-		// unset($_SESSION['errors']);
+	if(!empty($_SESSION['message'])) {
+		echo '<h2 class="green">' . $_SESSION['message'] . '</h2>';
+		session_destroy();
 	}
 	?>
 	<form action="process.php" method="post" enctype="multipart/form-data">
 		<h1>Registration Form</h1>
 		<h3>All fields with the * are manditory</h3>
-		<p>* Email: <input type="text" name="email"></p>
-		<p>* First Name: <input type="text" name="first_name"></p>
-		<p>* Last Name: <input type="text" name="last_name"></p>
-		<p>* Password: <input type="text" name="password"></p>
-		<p>* Confirm Password: <input type="text" name="password2"></p>
-		<p>Date of Birth: <input type="text" name="birthdate"></p>
+		<p>* Email: <input type="text" name="email" <?php echo $_SESSION['e'] ?>/p>
+		<p>* First Name: <input type="text" name="first_name" <?php echo $_SESSION['fn'] ?>/p>
+		<p>* Last Name: <input type="text" name="last_name" <?php echo $_SESSION['ln'] ?>/p>
+		<p>* Password: <input type="text" name="password" <?php echo $_SESSION['p'] ?>/p>
+		<p>* Confirm Password: <input type="text" name="password2" <?php echo $_SESSION['p'] ?>/p>
+		<p>Date of Birth: <input type="text" name="birthdate" <?php echo $_SESSION['b'] ?>/p>
 		<p>Upload a Profile Picture: <input type="file" name="picture"></p>
 		<input type="submit" name="submit" value="Register!">
 	</form>
 </body>
 </html>
+<?php 
+	unset($_SESSION['e']); 
+	unset($_SESSION['fn']);
+	unset($_SESSION['ln']);
+	unset($_SESSION['p']);
+	unset($_SESSION['b']);
+
+?>
